534a443a907c71256b354e6ca04471ca049f4962
keng <ohtakikenji@gmail.com>
2020-09-22 00:43:48 +0900

    add material-ui Album template

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-const App = () => {
-  return <>App Component</>;
+import HomePage from './components/pages/HomePage';
+
+const App: React.FC = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" component={HomePage} exact />
+      </Switch>
+    </Router>
+  );
 };
 
 export default App;
---
+++ frontend/src/components/pages/HomePage.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import Album from '../templates/Album';
+
+const HomePage: React.FC = () => {
+  return <Album />;
+};
+
+export default HomePage;
